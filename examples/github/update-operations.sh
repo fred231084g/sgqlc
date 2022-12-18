162157bd68e4276ee3460940ef1e1ee1af9306e8
fred231084g <freddy1984govoni@gmail.com>
2022-12-18 03:23:38 +0100

    Delete examples directory

--- examples/github/update-operations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sgqlc-codegen operation \
-    --schema github_schema.json \
-    github_schema \
-    sample_operations.py \
-    sample_operations.gql || exit 1
-
-python3 -c 'import sample_operations' || exit 1
--- examples/shopify/update-operations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sgqlc-codegen operation \
-    --schema shopify_schema.json \
-    shopify_schema \
-    shopify_operations.py \
-    shopify_operations.gql || exit 1
-
-python3 -c 'import shopify_operations' || exit 1
